cc98a298e4837e75e36271ab0f6f7b6f207dc701
GiaKhiem <peodz666@gmail.com>
2023-12-13 15:53:30 +0700

    Add project files.

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    [SerializeField] private LayerMask barrier;
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("DeadZone"))
+        {
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("Obstructions"))
+        {
+            if (!isBarrierDown())
+            {
+                gameObject.transform.parent = null;
+                StartCoroutine(DestroyObject());
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBarrierRight()&& !isBarrierDown())
+        {
+            gameObject.transform.parent = null;
+            StartCoroutine(DestroyObject());
+        }
+    }
+    
+    IEnumerator DestroyObject()
+    {
+        yield return new WaitForSeconds(2);
+        if(gameObject.transform.parent == null) 
+        {
+            Destroy(gameObject);
+        }
+    }
+    bool isBarrierRight()
+    {
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, barrier);
+        return raycastHit2D.collider != null;
+    }
+    bool isBarrierDown()
+    {
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, barrier);
+        return raycastHit2D.collider != null;
+    }
+
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField] private GameObject blockPrefab;
+    [SerializeField] private LayerMask barrier;
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D body;
+    private GameObject lastBlock;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * 5);
+        if (Input.GetMouseButtonDown(0))
+        {
+            // body.AddForce(Vector3.up);
+            //   block[isCount].SetActive(true);
+            //   isCount++;
+            //body.velocity = new Vector2(body.velocity.x, 5f);
+            Jump();
+            CreateBlockUnderPlayer();
+
+        }
+    }
+
+    void Jump()
+    {
+        // Đặt vận tốc Y để player nhảy lên
+         body.velocity = new Vector2(body.velocity.x, 5f);
+       // body.AddForce(new Vector3(0, 3, 0));
+    }
+
+    void CreateBlockUnderPlayer()
+    {
+        if (lastBlock == null)
+        {
+            // Nếu chưa có block nào, tạo block dưới player
+            lastBlock = Instantiate(blockPrefab, transform.position - new Vector3(0.1f, 1, 0), Quaternion.identity,transform);
+            lastBlock.SetActive(true);
+        }
+        else
+        {
+            // Nếu đã có block, tạo block mới ở dưới block trước đó
+            lastBlock.transform.position = lastBlock.transform.position - new Vector3(0, 0.7f, 0);
+            GameObject newBlock = Instantiate(blockPrefab, transform.position - new Vector3(0.1f, 1, 0), Quaternion.identity,transform);
+            lastBlock = newBlock; // Gán lastBlock là block vừa tạo
+            lastBlock.SetActive(true);
+        }
+    }
+    bool isBarrierRight()
+    {
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x,0), 0.1f, barrier);
+        return raycastHit2D.collider != null;
+    }
+    bool isBarrierDown()
+    {
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, barrier);
+        return raycastHit2D.collider != null;
+    }
+}
